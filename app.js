bbc0b7c58e35cf507c493f4f7f88a6104e7a13ef
gordonslondon <jules.boussekeyt@gmail.com>
2011-11-28 01:11:21 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,138 @@
+require.paths.unshift('vendor/mongoose/lib');
+
+var sio = require('socket.io')
+  , fs = require('fs')
+  , formidable = require('formidable')
+  , exec  = require('child_process').exec
+  , sys = require('sys')
+  , url = require('url')
+  , express = require('express')
+  , mongoose = require('mongoose')
+  , GridStore = require('mongodb').GridStore
+  , path = '/playlive'
+
+var app = express.createServer();
+
+app.configure(function () {
+    app.use('/public', express.static(__dirname + '/public'));
+    app.set('views', __dirname);
+    app.set('view engine', 'html');
+
+    this.use(express.cookieParser());
+    this.use(express.session({
+        "secret": "333333",
+        "store":  new express.session.MemoryStore({ reapInterval: 60000 * 10 })
+    }));
+});
+
+mongoose.connect('mongodb://localhost:27017/my_database');
+var Schema = mongoose.Schema
+  , ObjectId = Schema.ObjectId;
+
+var TrackSchema = new Schema({
+    id    : ObjectId
+  , title     : String
+  , file      : String
+});
+
+var Track = mongoose.model('Track', TrackSchema);
+
+function requireLogin (req, res, next) {
+    var queryString = url.parse(req.url,true);
+    if (queryString.query.user && queryString.query.user == 'grdn') {
+        req.session.auth = true;
+    }
+
+    if (req.session.auth) {
+        next();
+    } else {
+        // Otherwise, we redirect him to login form
+        res.statusCode = 403;
+        res.end('You must login');
+    }
+}
+
+app.register('.html', {
+    compile: function(str, options){
+        return {
+            call: function() {
+                return str;
+            }
+        };
+    }
+});
+
+app.get('/', [requireLogin], function (req, res) {
+    res.render('index', { layout: false });
+});
+
+app.listen(3000, function () {
+    var addr = app.address();
+    console.log('   app listening on http://' + addr.address + ':' + addr.port);
+});
+
+var io = sio.listen(app);
+
+var ALLOWED_MIME_TYPES = {
+    'audio/mpeg3': true,
+    'audio/mpeg': true
+};
+
+app.post('/upload', function(req, res) {
+    var basename = require('path').basename
+    , fName = basename(req.header('x-file-name'))
+    , mime = require('mime')
+    , ws = fs.createWriteStream('./public/music/'+fName);
+
+    var mimetype = mime.lookup(ws.path);
+
+    if (!ALLOWED_MIME_TYPES[mimetype]) {
+        console.log('### ERROR: INVALID MIMETYPE', mimetype);
+        res.end('INVALID MIMETYPE');
+        return;
+    }
+
+    req.on('data', function(data) {
+        ws.write(data);
+    });
+
+    var track = new Track();
+    track.title = fName;
+    track.file = fName;
+    track.save();
+
+    io.sockets.emit('add_track', track);
+});
+
+
+io.sockets.on('connection', function (socket) {
+
+    socket.on('create', function(data) {
+        var track = new Track();
+        track.title = data.title;
+        track.save();
+    });
+
+    socket.on('remove', function(data) {
+        Track.findById(data.id, function(err, track) {
+            if (!err) {
+                track.remove();
+                io.sockets.emit('remove_track', {id: data.id});
+            }
+        });
+    });
+
+    socket.on('play', function(data) {
+        io.sockets.emit('play_track', data);
+    });
+
+    socket.on('fetch_all', function(data) {
+        Track.find({}, function(err, tracks) {
+            console.log('tracks >>', tracks);
+            for (i = 0; i < tracks.length; i++) {
+                socket.emit('add_track', tracks[i]);
+            }
+        });
+    });
+
+});
---
+++ public/client.js
@@ -0,0 +1,192 @@
+var playlive = Backbone.View.extend({
+
+    el: $('body'),
+
+    initialize: function() {
+        this.status = $('.drop_zone .player_status');
+
+        this.socket = io.connect('http://localhost:3000');
+        this.socket.emit('fetch_all');
+
+        this.playlist = new jPlayerPlaylist({
+            jPlayer: "#jquery_jplayer_1",
+            cssSelectorAncestor: "#jp_container_1"
+        }, [], {
+            supplied: "mp3",
+            wmode: "window",
+            play: this._call('onPlay'),
+            pause: function() {
+                $('span.now_playing').remove();
+            },
+            playlistOptions: {
+                enableRemoveControls: true
+            }
+        });
+
+        this.player = $(this.playlist.cssSelector.jPlayer).jPlayer();
+
+        var context = this;
+
+        for (name in this._events) {
+            this.socket.on(name, this._call(this._events[name]));
+        }
+
+        dropZone = $('.drop_zone');
+        dropZone.removeClass('error');
+
+        // Check if window.FileReader exists to make 
+        // sure the browser supports file uploads
+        if (typeof(window.FileReader) == 'undefined') {
+            dropZone.text('Browser Not Supported!');
+            dropZone.addClass('error');
+            return;
+        }
+
+        // Add a nice drag effect
+        dropZone[0].ondragover = function () {
+            dropZone.addClass('hover');
+            return false;
+        };
+
+        // Remove the drag effect when stopping our drag
+        dropZone[0].ondragend = function () {
+            dropZone.removeClass('hover');
+            return false;
+        };
+
+        dropZone[0].ondrop = this._call('onDrop');
+    },
+
+    // call a method passing the player context
+    _call: function(method) {
+        var context = this;
+        return function() {
+            return context[method].apply(context, arguments);
+        };
+    },
+
+
+    // --- DOM events --- //
+
+    events: {
+        'click #jplayer_top_add': 'openUploadPane',
+        'click .jp-playlist-item-remove': 'remove',
+    },
+
+    openUploadPane: function(event) {
+        this.$('.drop_zone').show();
+    },
+
+    remove: function(e) {
+        var id = $(e.target).parents('li').first().data('id');
+        this.socket.emit('remove', {id: id});
+    },
+
+    onPlay: function(event) {
+        $('span.now_playing').remove();
+
+        var media = event.jPlayer.status.media;
+        $('li[data-id='+media.id+']').find('div').append(
+            $('<span/>').addClass('now_playing').text('[now playing]')
+        );
+    },
+
+
+    // --- SocketIO events --- //
+
+    _events: {
+        'add_track': 'addTrack',
+        'remove_track': 'removeTrack',
+    },
+
+    addTrack: function(track) {
+        this.playlist.add({
+            title: track.title,
+            mp3: '/public/music/'+track.file,
+            id: track._id
+        });
+    },
+
+    removeTrack: function(data) {
+        $('li[data-id='+data.id+']').remove();
+    },
+
+    // --- Upload --- //
+
+    onDrop: function(event) {
+        //Stop the browser from opening the file in the window
+        event.preventDefault();
+        dropZone.removeClass('hover');
+
+        // Get the file and the file reader
+        var file = event.dataTransfer.files[0];
+
+        // Validate file size
+        if(file.size > 30000000) {
+            dropZone.text('File Too Large!');
+            dropZone.addClass('error');
+            return false;
+        }
+
+        // Send the file
+        var xhr = new XMLHttpRequest();
+        xhr.upload.addEventListener('progress', this._call('uploadProgress'), false);
+        xhr.onreadystatechange = this.stateChange;
+        xhr.open('POST', '/upload', true);
+        xhr.setRequestHeader('X-FILE-NAME', file.name);
+        xhr.send(file);
+    },
+
+    uploadProgress: function(event) {
+        var percent = parseInt(event.loaded / event.total * 100);
+        this.status.text(percent == 100 ? '' : 'Uploading: ' + percent + '%');
+    },
+
+    // Show upload complete or upload failed depending on result
+    stateChange: function(event) {
+        var self = this;
+        if (event.target.readyState == 4) {
+            if (event.target.status == 200 || event.target.status == 304) {
+                self.status.text('Upload Complete!');
+            }
+            else {
+                dropZone.text('Upload Failed!');
+                dropZone.addClass('error');
+            }
+        }
+    }
+});
+
+jPlayerPlaylist.prototype._createListItem = function(media) {
+    self = this;
+
+    //Wrap the <li> contents in a <div>
+    var listItem = "<li data-id='"+media.id+"'><div>";
+
+    // Create remove control
+    listItem += "<a href='javascript:;' class='" + this.options.playlistOptions.removeItemClass + "'>&times;</a>";
+
+    // Create links to free media
+    if(media.free) {
+        var first = true;
+        listItem += "<span class='" + this.options.playlistOptions.freeGroupClass + "'>(";
+        $.each(media, function(property,value) {
+            if($.jPlayer.prototype.format[property]) { // Check property is a media format.
+                if(first) {
+                    first = false;
+                } else {
+                    listItem += " | ";
+                }
+                listItem += "<a class='" + self.options.playlistOptions.freeItemClass + "' href='" + value + "' tabindex='1'>" + property + "</a>";
+            }
+        });
+        listItem += ")</span>";
+    }
+
+    // The title is given next in the HTML otherwise the float:right on the free media corrupts in IE6/7
+    listItem += "<a href='javascript:;' class='" + this.options.playlistOptions.itemClass + "' tabindex='1'>" + media.title + (media.artist ? " <span class='jp-artist'>by " + media.artist + "</span>" : "") + "</a>";
+    listItem += "</div></li>";
+
+    return listItem;
+};
+
